e2ea9d477100dfa2aa50bdee508669e451842272
KapilModi656 <kapilmodi.656@gmail.com>
2026-07-05 10:31:05 +0530

    Add: greatest-common-divisor-traversal - submission-0

---
+++ Data Structures & Algorithms/greatest-common-divisor-traversal/submission-0.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    void dfs(map<int,vector<int>>& adj,vector<bool>& visited,int ind,vector<int>& nums){
+        int node = nums[ind];
+        visited[ind]=true;
+        for(int j=2;j*j<=node;j++){
+            if(node%j==0){
+                for(auto it:adj[j]){
+                    if(!visited[it]){
+                        dfs(adj,visited,it,nums);
+                    }
+                }
+            }
+            while(node%j==0){
+                node = node/j;
+            }
+        }
+        if(node!=1){
+            for(auto it:adj[node]){
+                if(!visited[it]){
+                    dfs(adj,visited,it,nums);
+                }
+            }
+        }
+    }
+    bool canTraverseAllPairs(vector<int>& nums) {
+        map<int,vector<int>> adj;
+        int n=nums.size();
+        for(int i=0;i<n;i++){
+            int num = nums[i];
+            for(int j=2;j*j<=nums[i];j++){
+                bool flag=false;
+                while(num%j==0){
+                    flag=true;
+                    num=num/j;
+                }
+                if(flag){
+                    adj[j].push_back(i);
+                }
+            }
+            if(num!=1){
+                adj[num].push_back(i);
+            }
+        }
+        vector<bool> visited(n,false);
+        dfs(adj,visited,0,nums);
+        for(auto it:visited){
+            if(it==false) return false;
+        }
+        return true;
+    }
+};
