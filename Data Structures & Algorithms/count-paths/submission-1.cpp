4c526d3f11031b941af0cc6ad1bbcdd06220237b
KapilModi656 <kapilmodi.656@gmail.com>
2026-07-15 19:58:50 +0530

    Add: count-paths - submission-1

---
+++ Data Structures & Algorithms/count-paths/submission-1.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        
+        vector<int> prev(n,0);
+        for(int i=0;i<m;i++){
+            vector<int> curr(n,0);  
+            if(i==0) curr[0]=1;
+            for(int j=0;j<n;j++){
+                if(i>0) curr[j]+=prev[j];
+                if(j>0) curr[j]+=curr[j-1];
+            }
+            swap(prev,curr);
+        }
+        return prev[n-1];
+    }
+};
