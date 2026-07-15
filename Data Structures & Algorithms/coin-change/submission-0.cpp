7045fbaca456663c75a54e64bedb15ce9fad7d8b
KapilModi656 <kapilmodi.656@gmail.com>
2026-07-15 09:59:40 +0530

    Add: coin-change - submission-0

---
+++ Data Structures & Algorithms/coin-change/submission-0.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int amount) {
+        int n=coins.size();
+        vector<int> dp(amount+1,INT_MAX);
+        dp[0]=0;
+        for(int i=0;i<n;i++){
+            for(int j=1;j<=amount;j++){
+                if(j<coins[i]) continue;
+                if(dp[j-coins[i]]==INT_MAX) continue;
+                dp[j] = min(dp[j],1+dp[j-coins[i]]);
+            }
+        }
+        if(dp[amount]==INT_MAX) return -1;
+        return dp[amount];
+    }
+};
